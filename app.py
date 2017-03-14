10622d8b1045c99913867057c387dc7434dcfe47
bryan.yang <bryan.yang@ringcentral.com>
2017-03-14 23:23:34 +0800

    handle exception

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ This file creates your application.
 
 import os
 import requests
-import urllib
+import urllib, urllib3
 import gzip
 import json
 import quadkey
@@ -64,16 +64,55 @@ class Station(db.Model):
 # Routing for your application.
 ###
 
-@app.route('/')
-def home():
-    """Render website's home page."""
-    return render_template('home.html')
+body = {
+    "code": 0,
+    "result": []
+}
 
 
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html')
+@app.route('/v1/ubike-station/taipei', methods=['GET'])
+def get_station():
+    input = request.args.to_dict() 
+    if 'lat' not in input.keys() or 'lng' not in input.keys():
+        body['code'] = -1
+        return jsonify(body)
+
+    elif input['lat'] is None or input['lng'] is None:
+        body['code'] = -1
+        return jsonify(body)
+
+    latlng = str(input['lat'] + ',' + input['lng'])
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='
+    url = url + latlng
+    http = urllib3.PoolManager()
+    
+    try:
+        r = http.request('GET',url, timeout = 1.0)
+    except Exception as e:
+        body['code'] = -3
+        return jsonify(body)    
+    
+    if r.status != 200:
+        body['code'] = -3
+    else:
+        res = json.loads(r.data)
+        if res['status'] != 'OK':
+            body['code'] = -1
+        else:
+            country = -1
+            county = -1
+            country = res["results"][-1]["address_components"][0]["long_name"]
+            try:
+                county = res["results"][0]["address_components"][4]["long_name"]
+            except Exception as e:
+                pass
+            print country, county
+            if country != "Taiwan" or county != "Taipei City":
+                body['code'] = -2
+            else:
+                body['code'] = 0
+                    
+    return jsonify(body)
 
 ###
 # Update DB
@@ -95,13 +134,6 @@ def update():
 ###
 # The functions below should be applicable to all Flask apps.
 ###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
 
 @app.after_request
 def add_header(response):
