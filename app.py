93a17f36b93cf4c6a2733e86a84913d3a3b35d7c
bryan.yang <bryan.yang@ringcentral.com>
2017-03-14 21:20:27 +0800

    add Station

--- app.py
+++ app.py
@@ -7,12 +7,58 @@ This file creates your application.
 """
 
 import os
-from flask import Flask, render_template, request, redirect, url_for
+import requests
+import urllib
+import gzip
+import json
+import quadkey
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/ubike'
+db = SQLAlchemy(app)
 
+###
+# donwnload ubike data
+###
+
+def download_ubike():
+    url = "https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.gz"
+    urllib.urlretrieve(url, "data/ubike.gz")
+    f = gzip.open('data/ubike.gz', 'r')
+    jdata = f.read()
+    f.close()
+    data = json.loads(jdata)
+    return data
+
+###
+# Create DB Model
+###
+
+# Station
+class Station(db.Model):
+    __tablename__ = "stations"
+    sno = db.Column(db.String(10), primary_key=True)
+    sna = db.Column(db.String(120), unique=True)
+    lat = db.Column(db.Float(10))
+    lng = db.Column(db.Float(10))
+    quadkey = db.Column(db.String(17), index=True)
+    mday = db.Column(db.BigInteger)
+
+    def __init__(self, sno, sna, lat, lng, quadkey, mday):
+        self.sno = sno
+        self.sna = sna
+        self.lat = lat
+        self.lng = lng
+        self.quadkey = quadkey
+        self.mday = mday
+
+    def __repr__(self):
+        return 'No: %s, Name: %s' % (self.sno, self. sna)
 
 ###
 # Routing for your application.
@@ -29,6 +75,22 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
+###
+# Update DB
+###
+
+@app.route('/update/', methods=['GET'])
+def update():
+    data = download_ubike()
+    for k, v in data['retVal'].iteritems():
+        if not db.session.query(Station).filter(Station.sno == v['sno']).count():
+            reg = Station(v['sno'], v['sna'], v['lat'], v['lng'], 
+                    str(quadkey.from_geo((v['lat'], v['lng']), 17)),  v['mday'])
+            db.session.add(reg)
+            db.session.commit()
+
+    updated = data['retVal']['0134']['mday']
+    return jsonify({'status': 'successed', 'updated': updated})
 
 ###
 # The functions below should be applicable to all Flask apps.
