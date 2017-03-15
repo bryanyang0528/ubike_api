d675eb968c1690ab1165c3c00abf9aca75581ba2
bryan.yang <bryan.yang@ringcentral.com>
2017-03-15 12:55:25 +0800

    adjust quadkey

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def find_stations(lat, lng):
     result = []
     unit = 14    
  
-    while len(dist) < 2 and unit > 8:
+    while len(dist) < 2 and unit >= 10:
         user_quadkey = str(quadkey.from_geo((lat, lng), unit))
         stations = Station.query.filter(Station.quadkey.like('%s%%' % user_quadkey)).all() 
     #print user_quadkey, stations
@@ -91,7 +91,7 @@ def find_stations(lat, lng):
         else:
             pass
 
-        print len(dist)
+        print len(dist), unit
         unit -= 1 
 
     return result
